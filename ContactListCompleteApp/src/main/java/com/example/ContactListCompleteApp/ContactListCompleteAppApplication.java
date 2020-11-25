d28379a3e81ac8eb629ad67b3c09111dde0e8dc4
Samira Sobhani <ss121280@yahoo.com>
2020-11-25 13:10:07 -0600

    add complete project

---
+++ ContactListCompleteApp/src/main/java/com/example/ContactListCompleteApp/ContactListCompleteAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.ContactListCompleteApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactListCompleteAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactListCompleteAppApplication.class, args);
+	}
+
+}
---
+++ ContactListCompleteApp/src/test/java/com/example/ContactListCompleteApp/ContactListCompleteAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ContactListCompleteApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactListCompleteAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
